eb4088c2d1600b8329946deb004a9b566ec8aa1b
sumanju <sumanman21296@gmail.com>
2020-03-08 21:49:57 +0530

    first deploy

--- app-server/src/routes/login.js
+++ app-server/src/routes/login.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router  = express.Router()
 
 router.post('/login', (req, res) => {
-  console.log('here');
-  
   console.log(req.body)
+  res.status(200).send({
+    pro1 : 'suman',
+    prop2 : 'mandal'
+  })
 })
 
 module.exports = router
