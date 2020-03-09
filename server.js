c02e4c50020394bbc1a2585462c783ebefad093c
sumanju <sumanman21296@gmail.com>
2020-03-09 09:10:34 +0530

    deploy on heroku

--- server.js
+++ server.js
@@ -9,17 +9,23 @@ const app = express()
       app.use(bodyParser.json())
       app.use(cors())
       app.use(routeList)
+ 
+      app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        )
+        next()
+      })
 
 app.use(express.static(path.join(__dirname, "./dist/angular/")))
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  next()
-})
+app.get('*', function(req,res) {
+    
+  res.sendFile(path.join(__dirname,'/dist/angular/index.html'));
+  });
+ 
 
 const port = process.env.PORT || 8080
 
