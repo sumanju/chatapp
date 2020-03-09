52385a24f49bd9b96826e2314c4da4e8a7b57a62
sumanju <sumanman21296@gmail.com>
2020-03-09 09:23:43 +0530

    deploy on heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser  = require('body-parser')
 const cors        = require('cors')
 const routeList   = require('./app-server/route-list')
 
-const dist = require('./')
+// const dist = require('./')
 const app = express()
       app.use(bodyParser.json())
       app.use(cors())
