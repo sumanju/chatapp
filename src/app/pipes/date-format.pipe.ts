43e690fb8b562b8f1e8d34beac0a887bf96b79bb
sumanju <sumanman21296@gmail.com>
2020-07-11 23:12:11 +0530

    new feature date

---
+++ src/app/pipes/date-format.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormat'
+})
+export class DateFormatPipe implements PipeTransform {
+
+  transform(dateTs : Date): any {
+    const seconds : number = (new Date(dateTs).getTime())/1000
+
+    if  ((Date.now()/1000) - seconds < 60)
+      return  ((Date.now()/1000) - seconds) + 's'
+    if  ((Date.now()/1000) - seconds < 3600)
+      return  (((Date.now()/1000) - seconds)/60).toFixed(0) + 'm'
+    if  ((Date.now()/1000) - seconds < 86400) 
+      return  (((Date.now()/1000) - seconds)/(60*60)).toFixed(0) + 'h'
+
+    return new Date(dateTs).toDateString()
+  }
+
+}
