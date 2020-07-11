4a6b6e5aa582a24e1ba35cc322b35359d4195059
sumanju <sumanman21296@gmail.com>
2020-07-11 23:30:42 +0530

    date pipe issue

--- src/app/pipes/date-format.pipe.ts
+++ src/app/pipes/date-format.pipe.ts
@@ -9,7 +9,7 @@ export class DateFormatPipe implements PipeTransform {
     const seconds : number = (new Date(dateTs).getTime())/1000
 
     if  ((Date.now()/1000) - seconds < 60)
-      return  ((Date.now()/1000) - seconds) + 's'
+      return  ((Date.now()/1000) - seconds).toFixed(0) + 's'
     if  ((Date.now()/1000) - seconds < 3600)
       return  (((Date.now()/1000) - seconds)/60).toFixed(0) + 'm'
     if  ((Date.now()/1000) - seconds < 86400) 
